a8de7b442f269b1ba8764ede5c69bab09d435fde
taxisti.ly@gmail.com <taxisti.ly@gmail.com>
2020-05-06 17:06:46 +0530

    driverDetail api

--- app/Repositories/Api/App/DriverRepository.php
+++ app/Repositories/Api/App/DriverRepository.php
@@ -90,8 +90,9 @@ class DriverRepository extends Controller
         if($request['type'] =='add_driver')
         {
 
-            $check_driver = Driver::find($request['user_id']);
-            if($check_driver)
+            $check_driver = Driver::where('driver_id',$request['user_id'])->first();
+
+            if(!$check_driver)
             {
                 // profile_pic handling 
                 if($request->file('profile_pic')){
@@ -127,62 +128,24 @@ class DriverRepository extends Controller
                 $driver['car_pic'] = '';
                 $driver['created_datetime'] = date('Y-m-d h:i:s');
         
-                if($driver_id!='' && $licence!='')
+                $add_driver_detail = Driver::insert($driver);
+                
+                if($add_driver_detail)
                 {
                     
-                    $add_driver_detail = Driver::insert($driver);
-                    
-                        $msg['data']['driver_detail_id']=$lst_id;
-                        $qry_get_driver_details=$con->query("select * from taxi_driver_detail where id=".$lst_id);
-                        if($qry_get_driver_details->num_rows>0)
-                        {
+                    $input['user_type'] = 1;
+                    $input['status'] = 0;
+                    $input['updated_date'] = date('Y-m-d h:i:s');
+                    isset($request['dob']) ? $input['date_of_birth'] = $request['dob'] : '';
+                    isset($request['first_name']) ? $input['first_name'] = $request['first_name'] : '';
+                    isset($request['last_name']) ? $input['last_name'] = $request['last_name'] : '';
+                    isset($request['password']) ? $input['password'] = md5($request['password']) : '';
 
-                            $input['user_type'] = 1;
-                            $input['status'] = 0;
-                            isset($request['dob']) ? $input['date_of_birth'] = $request['dob'] : '';
-                            isset($request['first_name']) ? $input['first_name'] = $request['first_name'] : '';
-                            isset($request['last_name']) ? $input['last_name'] = $request['last_name'] : '';
-                            isset($request['password']) ? $input['password'] = md5($request['password']) : '';
-
-        
-                            $up_qry.="where user_id=".$driver_id;
-        
-                            
-        
-                            $qry_update_user=$con->query($up_qry);
-                            
-                            $row_driver=$qry_get_driver_details->fetch_assoc();
-                            
-                            $qry_user=$con->query("select * from taxi_users where user_id=".$driver_id);
-                            
-                            if($qry_user->num_rows>0)
-                            {
-                                $row_user=$qry_user->fetch_assoc();
-                                $row_driver['dob']=$row_user['date_of_birth'];
-                                $row_driver['first_name']=$row_user['first_name'];
-                                $row_driver['last_name']=$row_user['last_name'];
-                                $row_driver['driver_ratting']=(string)round($driver_ratting['avgrating'],1);
-        
-                            }
-        
-                            if($row_driver['licence']!='')
-                            {
-                                $row_driver['licence']=$server_path.$row_driver['licence'];
-                            }
-        
-                            if($row_driver['profile']!='')
-                            {
-                                $row_driver['profile']=$server_path.$row_driver['profile'];					
-                            }
-                            $msg['data']=$row_driver;
-        
-                        }
-                        
-                        
-        
-                        $msg['message']="Driver Details Added Successfully.";
-                        $msg['message_ar'] = "تم إضافة معلومات السائق بنجاح";
-                        $msg['status']=1;
+                    $update_user = User::where('user_id',$request['user_id'])->update($input);
+                     
+                    $msg['message']="Driver Details Added Successfully.";
+                    $msg['message_ar'] = "تم إضافة معلومات السائق بنجاح";
+                    $msg['status']=1;
 
                 }
                 else{
@@ -199,22 +162,178 @@ class DriverRepository extends Controller
             }
         
         }
-        if($driver)
+        elseif($request['type']=='add_car_images')
         {
-            return response()->json([
-                'status'    => true,
-                'message'   => 'Success', 
-                'data'    => $driver['verify'],
-            ], 200);
+            
+            if(isset($request['driver_detail_id']) && $request['driver_detail_id']!='')
+            {
+                // car_image handling 
+                if($request->file('car_pic'))
+                {
+                    
+                    foreach ($request->file('car_pic') as  $car_image) {
+    
+                        // $car_image = $request->file('car_image');
+                        $imageName = 'uploads/car_images/'.time().'.'.$car_image->getClientOriginalExtension();
+                        $img = Storage::disk('s3')->put($imageName, file_get_contents($car_image), 'public');
+    
+                        $car['driver_detail_id'] = $request['driver_detail_id'];  
+                        $car['image'] = $imageName;  
+                        $car['datetime'] = date('Y-m-d H:m:s');  
+                        $car_image = DB::table('taxi_car_images')->insert($car);
+                    }
+
+                    $msg['message']="Car Image Added Successfully.";
+                    $msg['message_ar'] = "تم تحميل صورة السيارة بنجاح";
+                    $msg['status']=1;
+        
+                }
+                else
+                {
+                    $msg['message']="Failed.";
+                    $msg['status']=2;
+                }
+                
+            }
+            else
+            {
+                $msg['message']="Failed.";
+                $msg['status']=2;
+            }
         }
-        else
+        elseif($request['type']=='edit_driver')
         {
-            return response()->json([
-                'status'    => false,
-                'message'   => 'Driver not exist', 
-                'data'    => array(),
-            ], 200);
+            $driver_detail = Driver::where('driver_id',$request['user_id'])->first();
+
+            // profile_pic handling 
+            if($request->file('profile_pic')){
+
+                // delete files
+                Storage::disk('s3')->exists($driver_detail->profile) ? Storage::disk('s3')->delete($driver_detail->profile) : '';
+
+                $profile_pic = $request->file('profile_pic');
+                $imageName = 'uploads/driver_images/'.time().'.'.$profile_pic->getClientOriginalExtension();
+                $img = Storage::disk('s3')->put($imageName, file_get_contents($profile_pic), 'public');
+
+                $input['profile_pic'] = $imageName;
+                $driver['profile'] = $imageName;
+                                        
+            }
+
+            // licence handling 
+            if($request->file('licence')){
+
+                // delete files
+                Storage::disk('s3')->exists($driver_detail->licence) ? Storage::disk('s3')->delete($driver_detail->licence) : '';
+
+                $licence = $request->file('licence');
+                $imageName = 'uploads/licence_images/'.time().'.'.$licence->getClientOriginalExtension();
+                $img = Storage::disk('s3')->put($imageName, file_get_contents($licence), 'public');
+
+                $driver['licence'] = $imageName;
+                                        
+            }
+
+            isset($request['user_id']) ? $driver['driver_id'] = $request['user_id'] : '';
+            isset($request['car_brand']) ? $driver['car_brand'] = $request['car_brand'] : '';
+            isset($request['car_year']) ? $driver['car_year'] = $request['car_year'] : '';
+            isset($request['plate_no']) ? $driver['plate_no'] = $request['plate_no'] : '';
+            isset($request['availability']) ? $driver['availability'] = $request['availability'] : '';
+            isset($request['current_location']) ? $driver['current_location'] = $request['current_location'] : '';
+            isset($request['lat']) ? $driver['latitude'] = $request['lat'] : '';
+            isset($request['long']) ? $driver['longitude'] = $request['long'] : '';
+            $driver['last_update'] = date('Y-m-d h:i:s');
+    
+            $update_driver_detail = Driver::where('driver_id',$request['user_id'])->update($driver);
+            
+            if($update_driver_detail)
+            {
+                
+                $input['user_type'] = 1;
+                $input['status'] = 0;
+                $input['updated_date'] = date('Y-m-d h:i:s');
+                isset($request['dob']) ? $input['date_of_birth'] = $request['dob'] : '';
+                isset($request['first_name']) ? $input['first_name'] = $request['first_name'] : '';
+                isset($request['last_name']) ? $input['last_name'] = $request['last_name'] : '';
+
+                $update_user = User::where('user_id',$request['user_id'])->update($input);
+                    
+                $msg['message']="Driver Details Updated.";
+                $msg['message_ar'] = "تم تحميل معلومات السائق بنجاح";
+                $msg['status']=1;
+    
+
+            }
+            else{
+    
+                $msg['message']="Failed.";
+                $msg['status']=2;
+            }
+        
         }
+        elseif($request['type']=='get_driver')
+        {
+            $driver = DB::table('taxi_driver_detail')
+            ->select('taxi_driver_detail.*','taxi_users.*')
+            ->join('taxi_users', 'taxi_driver_detail.driver_id', '=', 'taxi_users.user_id')
+            ->where('taxi_driver_detail.driver_id',$request['user_id'])
+            ->first();
+    
+            if($driver)
+            {
+                $driver->licence = $driver->licence != ''? env('AWS_S3_URL').$driver->licence : '';
+                $driver->profile = $driver->profile != ''? env('AWS_S3_URL').$driver->profile : '';
+                $driver->profile_pic = $driver->profile_pic != ''? env('AWS_S3_URL').$driver->profile_pic : '';
+                
+                // add ratting
+                $driver->ratting = $this->get_driver_ratting($driver->driver_id);   
+                // add car images
+                $driver->car_images = $this->car_images($driver->id);
+
+                $msg['status']=1;
+                $msg['message']="Success";
+                $msg['data']=$driver;
+        
+            }
+            else
+            {
+                $msg['message']="Failed.";
+                $msg['status']=2;
+            }
+        }
+        elseif($request['type']=='get_driver_all_detail')
+        {
+            $driver = DB::table('taxi_driver_detail')
+            ->select('taxi_driver_detail.*','taxi_users.*')
+            ->join('taxi_users', 'taxi_driver_detail.driver_id', '=', 'taxi_users.user_id')
+            ->where('taxi_driver_detail.driver_id',$request['user_id'])
+            ->first();
+    
+            if($driver)
+            {
+                $driver->licence = $driver->licence != ''? env('AWS_S3_URL').$driver->licence : '';
+                $driver->profile = $driver->profile != ''? env('AWS_S3_URL').$driver->profile : '';
+                $driver->profile_pic = $driver->profile_pic != ''? env('AWS_S3_URL').$driver->profile_pic : '';
+                
+                // add ratting
+                $driver->ratting = $this->get_driver_ratting($driver->driver_id);   
+                // add car images
+                $driver->car_images = $this->car_images($driver->id);
+
+                $msg['status']=1;
+                $msg['message']="Success";
+                $msg['data']=$driver;
+        
+            }
+            else
+            {
+                $msg['message']="Failed.";
+                $msg['status']=2;
+            }
+        }
+        
+        return response()->json($msg, 200);
+
     }
 
 
